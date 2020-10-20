023d67a68568c54b2545c24a73d9512e2ea1fa03
HGK27 <62169668+HGK27@users.noreply.github.com>
2020-10-20 18:07:20 +0300

    refactoring
    ...

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp')
 var sass = require('gulp-sass');
 var nodemon = require('gulp-nodemon')
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('merhaba', function () {
   return gulp.src('./scss/main.scss')
     .pipe(sass())
+    .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest('./'))
 })
 gulp.task('start', function () {
